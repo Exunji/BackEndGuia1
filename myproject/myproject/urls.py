991a31f9964141f65af2eec618ffa91caf2e5379
Exunji <cristobalignacio@outlook.com>
2024-09-03 14:56:45 -0400

    ramitu vistas

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from ramitu1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ramitu1/', include("ramitu1.urls")),
 ]
